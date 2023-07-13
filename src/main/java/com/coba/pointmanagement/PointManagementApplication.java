f8bb860d4b98d413e42be3795191ee30cbd8f793
kcat2201 <kcat2201@gmail.com>
2023-07-13 18:27:27 +0900

    Initialize
    프로젝트 셋업

---
+++ src/main/java/com/coba/pointmanagement/PointManagementApplication.java
@@ -0,0 +1,16 @@
+package com.coba.pointmanagement;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class PointManagementApplication {
+
+    public static void main(String[] args) {
+        log.info("application arguments : " + String.join(",", args));
+        SpringApplication.run(PointManagementApplication.class, args);
+    }
+
+}
